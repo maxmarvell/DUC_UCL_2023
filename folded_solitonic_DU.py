4a4561b649eca4efa1a993dfbdcfdf670d38766e
maxmarvell <max.marvell@hotmail.co.uk>
2023-07-31 19:00:54 +0100

    resolving data

--- folded_solitonic_DU.py
+++ folded_solitonic_DU.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.optimize import minimize
+from time import time
 import random
 import sys
 
@@ -8,11 +9,56 @@ def main():
     seed_value = random.randrange(sys.maxsize)
     random.seed(seed_value)
 
+    start = time()
+    print("\n")
+
     q = 2
+    res = constrained_DU(q)
 
-    W, res = constrained_DU(q)
+    end = time()
+    print(end-start)
+    print("\n")
 
-    np.savetxt(f"./data/FoldedTensors/DU_{q}_{seed_value}.csv",W.reshape(q**4,q**4),delimiter=",")
+    W = np.full(shape=[q**2,q**2,q**2,q**2],fill_value=np.nan,dtype='complex_')
+
+    W[0,0,0,0] = 1
+
+    for i in  range(q**2):
+        for j in range(q**2):
+            if i != 0 or j != 0: 
+                W[i,j,0,0] = 0
+                W[i,0,j,0] = 0
+                W[0,0,i,j] = 0
+                W[0,i,0,j] = 0
+
+    indicies = np.argwhere(np.isnan(W))
+
+    for index, i in zip(indicies,res):
+            W[*index] = i
+
+    print(W)
+    print("\n")
+
+    norm = np.linalg.norm(W)
+    II = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
+    unitarity = np.linalg.norm(np.einsum('abcd,efcd -> abef', W, np.conj(W))-II)
+    dual_unitarity = np.linalg.norm(np.einsum('fbea,fdec->abcd',np.conj(W),W)-II)
+
+    I, Z = np.zeros(q**2), np.zeros(q**2)
+    I[0], Z[1] = 1, 1
+    IZ = np.einsum('a,b->ab',I,Z)
+    ZI = np.einsum('a,b->ab',Z,I)
+    soliton = np.linalg.norm(np.einsum('abcd,cd->ab',W,ZI)-IZ) + np.linalg.norm(np.einsum('abcd,cd->ab',W,IZ)-ZI)
+
+    print("Norm is:", norm)
+    print("Normalised? (i.e. norm/q = 1?):", np.linalg.norm(W)/(q**2))
+    print("Check unitarity: ", unitarity)
+    print("Check dual unitarity: ", dual_unitarity)
+
+    print("Check Z is a soliton: ",soliton)
+    
+    if unitarity < 1e-03 and dual_unitarity < 1e-03 and soliton < 1e-03 and abs(norm - (q**2)) < 1e-3:
+        np.savetxt(f"./data/FoldedTensors/DU_{q}_{seed_value}.csv",W.reshape(q**4,q**4),delimiter=",")
 
 def real_to_complex(z):
     return z[:len(z)//2] + 1j * z[len(z)//2:]
@@ -22,9 +68,11 @@ def complex_to_real(z):
 
 def constrained_DU(q:int):
 
+    empty = q**8 - (4*q**4 - 4*q**2 + 1)
+
     W = np.full(shape=[q**2,q**2,q**2,q**2],fill_value=np.nan,dtype='complex_')
-    R = real_to_complex(np.random.rand(2*(q**8 - (4*q**4 - 4*q**2 + 1))))
-    x0 = np.random.rand(2*(q**8 - (4*q**4 - 4*q**2 + 1)))
+    R = real_to_complex(np.random.rand(2*empty))
+    x0 = np.random.rand(2*empty)
 
     W[0,0,0,0] = 1
 
@@ -38,7 +86,7 @@ def constrained_DU(q:int):
 
     indicies = np.argwhere(np.isnan(W))
     
-    def unitarity(x:np.ndarray,W:np.ndarray,q:int):
+    def unitarity(x:np.ndarray,W:np.ndarray,q:int,indicies:np.ndarray):
 
         for index, i in zip(indicies,x):
             W[*index] = i
@@ -47,7 +95,7 @@ def constrained_DU(q:int):
 
         return np.linalg.norm(np.einsum('abcd,efcd -> abef', W, np.conj(W))-II)
     
-    def dual_unitarity(x:np.ndarray,W:np.ndarray,q:int):
+    def dual_unitarity(x:np.ndarray,W:np.ndarray,q:int,indicies:np.ndarray):
 
         for index, i in zip(indicies,x):
             W[*index] = i
@@ -56,29 +104,27 @@ def constrained_DU(q:int):
 
         return np.linalg.norm(np.einsum('fbea,fdec->abcd',np.conj(W),W)-II)
     
-    def soliton(x:np.ndarray,W:np.ndarray,q:int):
+    def soliton(x:np.ndarray,W:np.ndarray,q:int,indicies:np.ndarray):
 
         for index, i in zip(indicies,x):
             W[*index] = i
 
-        I, Z = np.identity(q**2), np.zeros([q**2,q**2])
-        Z[0,0], Z[1,1] = 1, -1
+        I, Z = np.zeros(q**2), np.zeros(q**2)
 
-        IZ = np.einsum('ac,bd->abcd',I,Z)
-        ZI = np.einsum('ac,bd->abcd',Z,I)
+        I[0], Z[1] = 1, 1
 
-        return np.linalg.norm(
-            np.einsum('abcd,cdef->abef',W,IZ)+
-            np.einsum('abcd,cdef->abef',ZI,W)-(IZ+ZI)
-        )
+        IZ = np.einsum('a,b->ab',I,Z)
+        ZI = np.einsum('a,b->ab',Z,I)
+
+        return np.linalg.norm(np.einsum('abcd,cd->ab',W,ZI)-IZ) + np.linalg.norm(np.einsum('abcd,cd->ab',W,IZ)-ZI)
     
-    def randomise(x:np.ndarray, R:np.ndarray):
-        return -abs(np.einsum("a,a->", x, R))
+    def randomise(X:np.ndarray, R:np.ndarray):
+        return -abs(np.einsum("a,a->", np.conj(X), R))
 
     cons = [
-            {'type':'eq', 'fun': lambda z:  unitarity(real_to_complex(z),W,q)}, 
-            {'type':'eq', 'fun': lambda z: dual_unitarity(real_to_complex(z),W,q)},
-            {'type':'eq', 'fun': lambda z: soliton(real_to_complex(z),W,q)}
+            {'type':'eq', 'fun': lambda z:  unitarity(real_to_complex(z),W,q,indicies)}, 
+            {'type':'eq', 'fun': lambda z: dual_unitarity(real_to_complex(z),W,q,indicies)},
+            {'type':'eq', 'fun': lambda z: soliton(real_to_complex(z),W,q,indicies)}
             ]
     
     res = minimize(
@@ -89,10 +135,10 @@ def constrained_DU(q:int):
         options={'maxiter': 1000},
         )
     
-    return W, res
+    return real_to_complex(res.x)
 
 if __name__ == "__main__":
-    for _ in range(100):
+    for _ in range(20):
         main()
 
 # class FoldedDUwithSoliton:
