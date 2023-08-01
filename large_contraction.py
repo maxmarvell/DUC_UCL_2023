186421f8496507b4a75ea92549a97d0dbab38627
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-01 12:07:34 +0100

    add skeleton function

--- large_contraction.py
+++ large_contraction.py
@@ -1,20 +1,23 @@
 import numpy as np
+from time import time
 
 '''
     Computing the correlation function only where opertor a is initially localised to x 
     , where x is an integer, and operator b is localised to some point y at time 2t, 
     where y is an integer.
 
-    TODO: 1. Need to add a function that computes skeletal diagram using transfer_matrix()
-          2. maybe implement random walk here
+    TODO: 1. maybe implement random walk here
 
     LATER: 1. Possible SVD for larger transfer matricies??
 '''
 
 def main():
     data = []
-    list_generator(20,data,3)
-    print(data)
+    start = time()
+    list_generator(20,data)
+    end = time()
+    print(len(data))
+    print(f"time to run: {end-start}s")
 
 def transfer_matrix(W: np.ndarray, a: np.ndarray, x: int,
                     horizontal: bool = True, terminate: bool = False):
@@ -43,8 +46,22 @@ def transfer_matrix(W: np.ndarray, a: np.ndarray, x: int,
     if not terminate:
         return np.einsum('ba,a->b',defect,p)
     else:
-        return np.einsum('a,a->',a,p)    
+        return np.einsum('a,a->',a,p)
+    
+def skeleton(x_h:np.ndarray, x_v:np.ndarray, W:np.ndarray,
+             a:np.ndarray, b:np.ndarray):
+    '''
+        Computes a contribution to the path integral of the
+        input skeleton diagram
+    '''
 
+    c = len(x_h)
+
+    for i in range(c-1):
+        a = transfer_matrix(W,a,x_h[i])
+        a = transfer_matrix(W,a,x_v[i],horizontal=False)
+
+    return transfer_matrix(W,a,x_h[-1],terminate=True)
 
 def list_generator(x:int,data:np.ndarray=[],
                    k:int=np.inf,lists:np.ndarray=[]):
