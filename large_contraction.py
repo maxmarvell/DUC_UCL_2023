ee181edcf58000b612da5c044f596fa3b9c4240f
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-03 01:17:26 +0100

    metropolis hastings

--- large_contraction.py
+++ large_contraction.py
@@ -1,15 +1,19 @@
 import numpy as np
 from time import time
+import random
+import sys
 
 '''
     Computing the correlation function only where opertor a is initially localised to x 
     , where x is an integer, and operator b is localised to some point y at time 2t, 
     where y is an integer.
 
-    TODO: 1. maybe implement random walk here
+    TODO: '1. maybe implement random walk here
 
     LATER: 1. Possible SVD for larger transfer matricies??
 '''
+
+random.seed(24)
 
 def main():
     data = []
@@ -19,8 +23,12 @@ def main():
     print(len(data))
     print(f"time to run: {end-start}s")
 
-def transfer_matrix(W: np.ndarray, a: np.ndarray, x: int,
-                    horizontal: bool = True, terminate: bool = False):
+    metropolis_hastings([],8,6)
+
+def transfer_matrix(W: np.ndarray, a: np.ndarray,
+                    x: int, b: np.ndarray = [],
+                    horizontal: bool = True,
+                    terminate: bool = False):
 
     """
         A transfer matrix can either be horizontal or vertical row of contracted
@@ -46,7 +54,7 @@ def transfer_matrix(W: np.ndarray, a: np.ndarray, x: int,
     if not terminate:
         return np.einsum('ba,a->b',defect,p)
     else:
-        return np.einsum('a,a->',a,p)
+        return np.einsum('a,a->',b,p)
     
 def skeleton(x_h:np.ndarray, x_v:np.ndarray, W:np.ndarray,
              a:np.ndarray, b:np.ndarray):
@@ -55,13 +63,11 @@ def skeleton(x_h:np.ndarray, x_v:np.ndarray, W:np.ndarray,
         input skeleton diagram
     '''
 
-    c = len(x_h)
-
-    for i in range(c-1):
+    for i in range(len(x_v)):
         a = transfer_matrix(W,a,x_h[i])
         a = transfer_matrix(W,a,x_v[i],horizontal=False)
 
-    return transfer_matrix(W,a,x_h[-1],terminate=True)
+    return transfer_matrix(W,a,x_h[-1],b=b,terminate=True)
 
 def list_generator(x:int,data:np.ndarray=[],
                    k:int=np.inf,lists:np.ndarray=[]):
@@ -84,8 +90,17 @@ def list_generator(x:int,data:np.ndarray=[],
         sublist.append(i)
         list_generator(x-i,data,k,sublist)
 
-def metropolis_hastings():
-    pass
+def metropolis_hastings(input:np.ndarray,
+                        x:int):
+
+    if input[-1] != x or input[0] != 0:
+        raise Exception('Input array not of correct format')
+    
+    for i in range(1,len(input)-1):
+         if random.randrange(sys.maxsize) % 2 and input[i-1]-input[i+1] == 0:
+            input[i] = 2*input[i+1] - input[i]
+
+    return input
     
 if __name__ == '__main__':
     main()
