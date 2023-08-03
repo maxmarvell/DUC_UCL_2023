2865e86fc30d4d593d0934866de74de895801da9
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-03 18:42:58 +0100

    Combining to general path integral

--- large_contraction.py
+++ large_contraction.py
@@ -2,6 +2,7 @@ import numpy as np
 from time import time
 import random
 import sys
+import math
 
 '''
     Computing the correlation function only where opertor a is initially localised to x 
@@ -13,82 +14,92 @@ import sys
     LATER: 1. Possible SVD for larger transfer matricies??
 '''
 
-random.seed(24)
-
 def main():
-    data = []
-    start = time()
-    list_generator(20,data)
-    end = time()
-    print(len(data))
-    print(f"time to run: {end-start}s")
 
-    metropolis_hastings([],8,6)
+    path_integral(3,8)
 
-def transfer_matrix(W: np.ndarray, a: np.ndarray,
-                    x: int, b: np.ndarray = [],
-                    horizontal: bool = True,
-                    terminate: bool = False):
+def path_integral(x:float,t:int):
 
-    """
-        A transfer matrix can either be horizontal or vertical row of contracted
-        folded tensors. 
-        For the case of a horizontal transfer matrix the row can only be terminated
-        either by a defect or by the operator b
+    def transfer_matrix(W: np.ndarray, a: np.ndarray,
+                        x: int, b: np.ndarray = [],
+                        horizontal: bool = True,
+                        terminate: bool = False):
 
-        a can be like [0,1,0,0]
-    """
+        """
+            A transfer matrix can either be horizontal or vertical row of contracted
+            folded tensors. 
+            For the case of a horizontal transfer matrix the row can only be terminated
+            either by a defect or by the operator b
 
-    if horizontal:
-        direct = W[0,:,:,0]
-        defect = W[:,:,0,0]
-    else:
-        direct = W[:,0,0,:]
-        defect = W[0,0,:,:]
+            a can be like [0,1,0,0]
+        """
 
-    p = np.einsum('ba,a->b',direct,a)
+        if horizontal:
+            direct = W[0,:,:,0]
+            defect = W[:,:,0,0]
+        else:
+            direct = W[:,0,0,:]
+            defect = W[0,0,:,:]
 
-    for _ in range(x-1):
-        p = np.einsum('ba,a->b',direct,p)
+        p = np.einsum('ba,a->b',direct,a)
 
-    if not terminate:
-        return np.einsum('ba,a->b',defect,p)
-    else:
-        return np.einsum('a,a->',b,p)
-    
-def skeleton(x_h:np.ndarray, x_v:np.ndarray, W:np.ndarray,
-             a:np.ndarray, b:np.ndarray):
-    '''
-        Computes a contribution to the path integral of the
-        input skeleton diagram
-    '''
+        for _ in range(x-1):
+            p = np.einsum('ba,a->b',direct,p)
 
-    for i in range(len(x_v)):
-        a = transfer_matrix(W,a,x_h[i])
-        a = transfer_matrix(W,a,x_v[i],horizontal=False)
+        if not terminate and not horizontal:
+            return np.einsum('ba,a->b',defect,p)
+        elif not terminate and horizontal:
+            return p
+        else:
+            return np.einsum('a,a->',b,p)
+        
+    def skeleton(x_h:np.ndarray, x_v:np.ndarray, W:np.ndarray,
+                a:np.ndarray, b:np.ndarray):
+        '''
+            Computes a contribution to the path integral of the
+            input skeleton diagram
+        '''
 
-    return transfer_matrix(W,a,x_h[-1],b=b,terminate=True)
+        for i in range(len(x_v)):
+            a = transfer_matrix(W,a,x_h[i])
+            a = transfer_matrix(W,a,x_v[i],horizontal=False)
 
-def list_generator(x:int,data:np.ndarray=[],
-                   k:int=np.inf,lists:np.ndarray=[]):
-    '''
-        Generates a complete set of possible lists which can
-        combine to form a complete set 
+        return transfer_matrix(W,a,x_h[-1],b=b,terminate=True)
 
-        For now just taking base case of the operator a being 
-        intially being localised to an integer position 
-    '''
+    def list_generator(x:int,data:np.ndarray,dict:dict,
+                    k:int=np.inf,lists:np.ndarray=[]):
+        '''
+            Generates a complete set of possible lists which can
+            combine to form a complete set 
 
-    if x == 0:
-        data.append(lists)
-        return
-    elif len(lists) >= k:
-        return 
+            For now just taking base case of the operator a being 
+            intially being localised to an integer position 
+        '''
 
-    for i in range(1,x+1):
-        sublist = lists.copy()
-        sublist.append(i)
-        list_generator(x-i,data,k,sublist)
+        if x == 0:
+            dict[len(data)] = len(lists)
+            data.append(lists)
+            return
+        elif len(lists) >= k:
+            return 
+
+        for i in range(1,x+1):
+            sublist = lists.copy()
+            sublist.append(i)
+            list_generator(x-i,data,dict,k,sublist)
+
+    vertical_data = []
+    vertical_dict = {}
+
+    horizontal_data = []
+    horizontal_dict = {}
+
+    x_h, x_v = t + math.ceil(x), t + 1 - math.ceil(x)
+
+    list_generator(x_v,vertical_data,vertical_dict)
+    list_generator(x_h,horizontal_data,horizontal_dict)
+
+    print(vertical_dict)
 
 def metropolis_hastings(input:np.ndarray,
                         x:int):
