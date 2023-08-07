bdae5bc5ec52198075fd001fc251e39906883aab
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-07 16:47:17 +0100

    adding loop to generate all data

--- W_generator.py
+++ W_generator.py
@@ -1,16 +1,64 @@
-import numpy as np
-from scipy.stats import unitary_group
 from scipy.optimize import minimize
+import numpy as np
+import random
 import time
+import sys
 
+'''
+    Optimisation routine that generates random dual unitary folded tensors
+    for dimension q.
+    Additional Soliton condition enforced.
+    Run to generate a seeded random value dual unitary which is saved as CSV
+'''
 
-def real_to_complex(z):      # real vector of length 2n -> complex of length n
+def real_to_complex(z):
     return z[:len(z)//2] + 1j * z[len(z)//2:]
 
-
-def complex_to_real(z):      # complex vector of length n -> real of length 2n
+def complex_to_real(z):
     return np.concatenate((np.real(z), np.imag(z)))
 
+def main():
+        
+    seed_value = random.randrange(sys.maxsize)
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+
+    start = time()
+    print("\n")
+
+    q = 2
+    W = initialize_W(q)
+    indices = np.argwhere(np.isnan(W))
+    results = find_W_constrained(q, soliton=True)
+    repack_W(indices,results,W)
+
+    end = time()
+    print(end-start)
+    print("\n")
+
+    print(W)
+    print("\n")
+
+    II = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
+    I, Z = np.zeros(q**2), np.zeros(q**2)
+    I[0], Z[1] = 1, 1
+    IZ = np.einsum('a,b->ab',I,Z)
+    ZI = np.einsum('a,b->ab',Z,I)
+    
+    N = np.linalg.norm(W)/(q**2)
+    print("Normalised:", np.linalg.norm(W)/(q**2))
+
+    U = np.linalg.norm(np.einsum('abcd,efcd -> abef', W, np.conj(W))-II)
+    print("Check unitarity: ", U)
+
+    DU = np.linalg.norm(np.einsum('fbea,fdec->abcd',np.conj(W),W)-II)
+    print("Check dual unitarity: ", DU)
+
+    S = np.linalg.norm(np.einsum('abcd,cd->ab',W,ZI)-IZ) + np.linalg.norm(np.einsum('abcd,cd->ab',W,IZ)-ZI)
+    print("Check Z is a soliton: ", S)
+    
+    if all([U,DU,S,abs(N - 1)] < 1e-3):
+        np.savetxt(f"./data/FoldedTensors/DU_{q}_{seed_value}.csv",W.reshape(q**4,q**4),delimiter=",")
 
 def initialize_W(q:int):
 
@@ -24,42 +72,40 @@ def initialize_W(q:int):
                 W[i,0,j,0] = 0
                 W[0,0,i,j] = 0
                 W[0,i,0,j] = 0
-        
-    return W
-    
+    return W 
 
 def repack_W(indices,X,W):
 
-    for i in range(len(X)):
-        index = indices[i]
-        e = X[i]
-        W[index[0],index[1],index[2],index[3]] = e
-        
+    if sys.version < 3.11:
+        for i in range(len(X)):
+            index = indices[i]
+            e = X[i]
+            W[index[0],index[1],index[2],index[3]] = e
 
+    else:
+        for index, i in zip(indices,X):
+                W[*index] = i
+        
 def randomise(X, R):
     return -abs(np.einsum("a,a->", np.conj(X), R))
 
+def find_W_constrained(q):
 
-def find_W_constrained(q, soliton = False, print_result = False):
-
-    Num_unfixed = q**8 - (4*q**4 - 4*q**2 + 1)
-    X_0 = np.random.rand(2*Num_unfixed)
-    R = real_to_complex(np.random.rand(2*Num_unfixed))
-
+    unfixed = q**8 - (4*q**4 - 4*q**2 + 1)
+    X_0 = np.random.rand(2*unfixed)
+    R = real_to_complex(np.random.rand(2*unfixed))
 
     def unitarity(indices,X,W):
 
         repack_W(indices,X,W)
-        IdId = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
-        return np.linalg.norm(np.einsum('abcd,efcd -> abef', W, np.conj(W)) - IdId)
-
+        II = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
+        return np.linalg.norm(np.einsum('abcd,efcd -> abef', W, np.conj(W)) - II)
 
     def dual_unitarity(indices,X,W):
-        
-        repack_W(indices,X,W)
-        IdId = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
-        return np.linalg.norm(np.einsum('fbea,fdec->abcd', np.conj(W), W)-IdId)
 
+        repack_W(indices,X,W)
+        II = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
+        return np.linalg.norm(np.einsum('fbea,fdec->abcd', np.conj(W), W)-II)
 
     def soliton(indices,X,W):
 
@@ -70,69 +116,20 @@ def find_W_constrained(q, soliton = False, print_result = False):
         ZI = np.einsum('a,b->ab',Z,I)
         return np.linalg.norm(np.einsum('abcd,cd->ab',W,ZI)-IZ) + np.linalg.norm(np.einsum('abcd,cd->ab',W,IZ)-ZI)
 
-
-
     W = initialize_W(q)
     indices = np.argwhere(np.isnan(W))
 
-    if not soliton:
-
-        cons = [{'type':'eq', 'fun': lambda z:  unitarity(indices, real_to_complex(z), W)},
-                {'type':'eq', 'fun': lambda z: dual_unitarity(indices, real_to_complex(z), W)}]
-
-    else:
-
-        cons = [{'type':'eq', 'fun': lambda z:  unitarity(indices, real_to_complex(z), W)},
-                {'type':'eq', 'fun': lambda z: dual_unitarity(indices, real_to_complex(z), W)},
-                {'type':'eq', 'fun': lambda z: soliton(indices, real_to_complex(z), W)}]
+    cons = [{'type':'eq', 'fun': lambda z:  unitarity(indices, real_to_complex(z), W)},
+            {'type':'eq', 'fun': lambda z: dual_unitarity(indices, real_to_complex(z), W)},
+            {'type':'eq', 'fun': lambda z: soliton(indices, real_to_complex(z), W)}]
 
     result = minimize(
         lambda z: randomise(real_to_complex(z), R), x0 = X_0, method='SLSQP', \
              constraints=cons, options={'maxiter': 1000}
                      )
-    
-    if print_result == True:
-        print(result)
         
     return real_to_complex(result.x)
 
-
-
-
-start = time.time()
-print("\n\n")
-
-q=2 #the local Hilbert space dimension
-W = initialize_W(q)
-indices = np.argwhere(np.isnan(W))
-results = find_W_constrained(q, soliton=True)
-repack_W(indices, results, W)
-
-end = time.time()
-print(end-start)
-print("\n")
-
-print(W)
-print("\n\n")
-
-print("Norm is:", np.linalg.norm(W))
-print("Normalised? (i.e. norm/q = 1?):", np.linalg.norm(W)/(q**2))
-IdId = np.einsum("ac,bd->abcd", np.identity(q**2), np.identity(q**2))
-
-print("Check unitarity: ", \
-      np.linalg.norm(np.einsum('abcd,efcd -> abef', W, np.conj(W))-IdId))
-
-print(
-      "Check dual unitarity: ", \
-      np.linalg.norm(np.einsum('fbea,fdec->abcd',np.conj(W),W)-IdId)
-      )
-
-I, Z = np.zeros(q**2), np.zeros(q**2)
-I[0], Z[1] = 1, 1
-IZ = np.einsum('a,b->ab',I,Z)
-ZI = np.einsum('a,b->ab',Z,I)
-
-print(
-      "Check Z is a soliton: ", \
-          np.linalg.norm(np.einsum('abcd,cd->ab',W,ZI)-IZ) + np.linalg.norm(np.einsum('abcd,cd->ab',W,IZ)-ZI)
-                  )
+if __name__ == "__main__":
+    for _ in range(20):
+        main()
