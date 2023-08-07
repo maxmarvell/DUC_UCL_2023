ebd23447fb6a2187a4827050a6aca56f32904d85
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-07 16:26:34 +0100

    adding loop to generate all data

--- P_generator.py
+++ P_generator.py
@@ -1,17 +1,69 @@
 import numpy as np
-from scipy.stats import unitary_group
 from scipy.optimize import minimize
 from scipy.linalg import expm 
+from W_generator import real_to_complex, complex_to_real
 import time
+import random
+import os
+import re
 
+def main():
 
-def real_to_complex(z):      # real vector of length 2n -> complex of length n
-    return z[:len(z)//2] + 1j * z[len(z)//2:]
+    for _, _, files in os.walk("data/FoldedTensors"):
+        for file in files:
 
+            q = 2
 
-def complex_to_real(z):      # complex vector of length n -> real of length 2n
-    return np.concatenate((np.real(z), np.imag(z)))
+            for e in range(0.01,0.1,0.01):
 
+                rstr = r'DU_' + str(q) + r'_([0-9]*).csv'
+                rx = re.compile(rstr)
+
+                res = rx.match(file)
+                seed_value = res.group(1)
+
+                random.seed(seed_value)
+                np.random.seed(seed_value)
+
+                P = find_P_constrained(q)
+                G = Exponential_Map(e, P)
+
+                start = time.time()
+                print("\n")
+
+                end = time.time()
+                print(end-start)
+                print("\n")
+
+                I, Z = np.zeros(q**2), np.zeros(q**2)
+                I[0], Z[1] = 1, 1
+
+                IZ = np.einsum('a,b->ab',I,Z).flatten()
+                ZI = np.einsum('a,b->ab',Z,I).flatten()
+                II = np.einsum('a,b->ab',I,I).flatten()
+
+                Q = IZ + ZI
+
+                H = np.linalg.norm(P - P.conj().T)
+                print("Check Hermiticity: ", H)
+
+                U1 = np.linalg.norm(np.einsum('ab,b->a',P,II))
+                print("Check Unitality: ", U1)
+
+                Q1 = np.linalg.norm(np.einsum('ab,b->a',P,Q))
+                print("Check Q-Conservation: ", Q1)
+
+                U2 = np.linalg.norm(np.einsum('ab,ac->bc', G, np.conj(G)) - np.eye(q**4))
+                print("Check Unitarity: ", U2)
+
+                U3 = np.linalg.norm(np.einsum('ab,b->a',G,II) - II)
+                print("Check Unitality: ", U3)
+
+                Q2 = np.linalg.norm(np.einsum('ab,b->a',G,Q) - Q)
+                print("Check Q-Conservation: ", Q2)
+                
+                if all([H,U1,Q1,U2,U3,Q2]<1e-3):
+                    np.savetxt(f"./data/FoldedPertubations/P_{q}_{e}_{seed_value}.csv",G.reshape(q**4,q**4),delimiter=",")
 
 def find_P_constrained(q, print_result = False):
 
@@ -22,17 +74,11 @@ def find_P_constrained(q, print_result = False):
     I[0], Z[1] = 1, 1
     IZ = np.einsum('a,b->ab',I,Z).flatten()[1:]
     ZI = np.einsum('a,b->ab',Z,I).flatten()[1:]
-    #II = np.einsum('a,b->ab',I,I).flatten()
 
     def Hermiticity(P):
         P = real_to_complex(P)
         P = P.reshape([q**4 - 1,q**4 - 1])
         return np.linalg.norm(P - P.conj().T)
-    
-    ####def Unitality(P):
-        ###P = real_to_complex(P)
-        ##P = P.reshape([q**4,q**4])
-        #return np.linalg.norm(np.einsum('ab,b->a',P,II))
     
     def Charge_conservation(P):
         P = real_to_complex(P)
@@ -44,10 +90,8 @@ def find_P_constrained(q, print_result = False):
         P = real_to_complex(P)
         return -abs(np.einsum("a,a->", np.conj(P), R))
 
-
     cons = [{'type':'eq', 'fun': lambda z:  Hermiticity(z)},
             {'type':'eq', 'fun': lambda z: Charge_conservation(z)}]
-
         
     result = minimize(
         lambda z: randomise(z, R), x0 = P_0, method='SLSQP', \
@@ -63,53 +107,16 @@ def find_P_constrained(q, print_result = False):
         
     return Direct_Sum
 
-
 def Exponential_Map(e, P):
     return expm(1j*e*P)
 
+def plot(G):
 
-start = time.time()
-print("\n")
+    import matplotlib.pyplot as plt
 
-q=2 #the local Hilbert space dimension
-e=0.1 #perturbation parameter
-P = find_P_constrained(q)
-I, Z = np.zeros(q**2), np.zeros(q**2)
-I[0], Z[1] = 1, 1
-IZ = np.einsum('a,b->ab',I,Z).flatten()
-ZI = np.einsum('a,b->ab',Z,I).flatten()
-II = np.einsum('a,b->ab',I,I).flatten()
-Q = IZ + ZI
-G = Exponential_Map(e, P)
+    plt.imshow(np.abs(G),cmap='hot', interpolation='nearest')
+    plt.show()
 
-end = time.time()
-print(end-start)
-print("\n")
-
-print(G)
-print("\n\n")
-
-print("Check Hermiticity: ", \
-      np.linalg.norm(P - P.conj().T))
-
-print("Check Unitality: ", \
-      np.linalg.norm(np.einsum('ab,b->a',P,II)))
-
-print("Check Q-Conservation: ", \
-      np.linalg.norm(np.einsum('ab,b->a',P,Q)))
-
-print("Check Unitarity: ", \
-      np.linalg.norm(np.einsum('ab,ac->bc', G, np.conj(G)) - np.eye(q**4)))
-
-print("Check Unitality: ", \
-      np.linalg.norm(np.einsum('ab,b->a',G,II) - II))
-
-print("Check Q-Conservation: ", \
-      np.linalg.norm(np.einsum('ab,b->a',G,Q) - Q))
-
-
-
-
-import matplotlib.pyplot as plt
-plt.imshow(np.abs(G),cmap='hot', interpolation='nearest')
-plt.show()
+if __name__ == "__main__":
+    for _ in range(20):
+        main()
