b64948c49695fdd754501ec6204ceb2c10e7b9bf
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-23 14:41:16 +0100

    adding precomputing SVD

--- path_integral_thicker.py
+++ path_integral_thicker.py
@@ -13,26 +13,24 @@ def main():
     P = np.loadtxt(f'data/FoldedPertubations/P_2_866021931.csv',delimiter=',',dtype='complex_')
     e = 1e-7
     q = 2
-    d = 3
+    d = 5
 
     G = Exponential_Map(e,P)
     PW = np.einsum('ab,bc->ac',G,W).reshape(q**2,q**2,q**2,q**2)
 
-    tiles = get_tiles(PW,d)
-
     I, Z = np.zeros(q**2,dtype='complex_'), np.zeros(q**2,dtype='complex_')
     I[0], Z[1] = 1, 1
 
-    a = np.einsum('a,b,c->abc',I,I,Z).reshape(-1)
-    b = np.einsum('a,b,c->abc',Z,I,I).reshape(-1)
+    a = np.einsum('a,b,c->abc',I,I,I,I,Z).reshape(-1)
+    b = np.einsum('a,b,c->abc',Z,I,I,I,I).reshape(-1)
 
     start = time()
-    val = path_integral(0.5,8.5,d,tiles,a,b,K=10)
+    val = path_integral(0.5,8.5,d,a,b,K=10)
     end = time()
     print(f'\nTruncated case: {val} computed in {end-start}s')
 
     # start = time()
-    # val = path_integral(0.5,8.5,d,tiles,a,b)
+    # val = path_integral(0.5,8.5,d,a,b)
     # end = time()
 
     # print(f'\nComplete case: {val} computed in {end-start}s')
@@ -44,7 +42,6 @@ def main():
 def path_integral(x:float,
                   t:float,
                   d:int,
-                  tiles:dict,
                   a:np.ndarray,
                   b:np.ndarray,
                   K:int = None):
@@ -61,50 +58,18 @@ def path_integral(x:float,
             either by a defect or by the operator b
         '''
 
-        if horizontal:
-            direct = tiles['h_direct']
-            defect = tiles['h_defect']
-        else:
-            direct = tiles['v_direct']
-            defect = tiles['v_defect']
-
-        if not terminate:
-            print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
-            for _ in range(l-1):
-                a = np.einsum('ab,b->a',direct,a)
-            return np.einsum('ab,b->a',defect,a)
-
-        elif terminate and (int(2*(t+x))%2 == 0):
-            print('    TERMINATING WITH DIRECT')
-            print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
-            for _ in range(l):
-                a = np.einsum('ab,b->a',direct,a)
-            return np.einsum('a,a->',a,b)
-
-        else:
-            print('      TERMINATING WITH DEFECT')
-            print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
-            for _ in range(l-1):
-                a = np.einsum('ab,b->a',direct,a)
-            a = np.einsum('ab,b->a',defect,a)
-            return np.einsum('a,a->',a,b)
+        def contract(a,A):
+            return np.einsum('ab,b->a',A,a)
         
-    def truncated_transfer_matrix(a:np.ndarray,
-                                  l:int,
-                                  horizontal:bool = True,
-                                  terminate:bool = False):
-        
-        '''
-            A transfer matrix can either be horizontal or vertical row of contracted
-            folded tensors. 
-            For the case of a horizontal transfer matrix the row can only be terminated
-            either by a defect or by the operator b
-        '''
-
-        def contract(a,u,s,v):
+        def truncated_contract(a,u,s,v):
             a = np.einsum('ab,b->a',v,a)
             a = np.multiply(s,a)
             return np.einsum('ab,b->a',u,a)
+        
+        if not K:
+            fun = contract
+        else:
+            fun = truncated_contract
 
         if horizontal:
             direct = tiles['h_direct']
@@ -113,33 +78,25 @@ def path_integral(x:float,
             direct = tiles['v_direct']
             defect = tiles['v_defect']
 
-        if K:
-            u1, s1, v1 = np.linalg.svd(direct)
-            u1, s1, v1 = u1[:,:K], s1[:K], v1[:K,:]
-
-            u2, s2, v2 = np.linalg.svd(defect)
-            u2, s2, v2 = u2[:,:K], s2[:K], v2[:K,:]
-
-
         if not terminate:
             print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
             for _ in range(l-1):
-                a = contract(a,u1,s1,v1)
-            return contract(a,u2,s2,v2)
+                a = fun(a,*direct)
+            return fun(a,*defect)
 
         elif terminate and (int(2*(t+x))%2 == 0):
             print('    TERMINATING WITH DIRECT')
             print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
             for _ in range(l):
-                a = contract(a,u1,s1,v1)
+                a = fun(a,*direct)
             return np.einsum('a,a->',a,b)
 
         else:
             print('      TERMINATING WITH DEFECT')
             print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
             for _ in range(l-1):
-                a = contract(a,u1,s1,v1)
-            a = contract(a,u2,s2,v2)
+                a = fun(a,*direct)
+            a = fun(*defect)
             return np.einsum('a,a->',a,b)
         
     def skeleton(h:np.ndarray,
@@ -150,22 +107,19 @@ def path_integral(x:float,
             input skeleton diagram
         '''
 
-        if not K:
-            fun = transfer_matrix
-        else:
-            fun = truncated_transfer_matrix
-
         for i in range(len(v)):
 
-            a = fun(a,h[i])
+            a = transfer_matrix(a,h[i])
 
             print(f'    Computed horizontal transfer matrix of length {h[i]}')
 
-            a = fun(a,v[i],horizontal=False)
+            a = transfer_matrix(a,v[i],horizontal=False)
 
             print(f'    Computed vertical transfer matrix of length {v[i]}')
 
-        return fun(a,h[-1],terminate=True) if len(h) > len(v) else np.einsum('a,a->',a,b)
+        return transfer_matrix(a,h[-1],terminate=True) if len(h) > len(v) else np.einsum('a,a->',a,b)
+    
+    tiles = tile_SVD(d,K)
     
     vertical_data = {}
     horizontal_data = {}
@@ -224,9 +178,15 @@ def tile_SVD(d:int,
     path = 'data/TileZoo'
     types = ['h_direct','h_defect','v_direct','v_defect']
 
-    for i in types:
-        tile = np.loadtxt(f'{path}/{i}_{d}x{d}',delimiter=',',dtype='complex_')
-        tiles[i] = rsvd(tile,K,q=4)
+    if not K:
+        for i in types:
+            tiles[i] = (np.loadtxt(f'{path}/{i}_{d}x{d}.csv',delimiter=',',dtype='complex_'),)
+
+    else:
+        for i in types:
+            tile = np.loadtxt(f'{path}/{i}_{d}x{d}.csv',delimiter=',',dtype='complex_')
+            U, S, V = np.linalg.svd(tile)
+            tiles[i] = (U[:,:K],S[:K],V[:K,:],)
 
     return tiles
 
@@ -266,7 +226,6 @@ def get_tiles(W:np.ndarray,
 
     reshape = inds1 + inds2
     val = val.transpose(*reshape,inplace=True)
-    print(val)
 
     h_direct = val.data
     h_direct = h_direct.reshape(-1,*h_direct.shape[-d:])
@@ -298,7 +257,6 @@ def get_tiles(W:np.ndarray,
         reshape = reshape + (f'k{2*i+1},{2*d}',)
 
     val = val.transpose(*reshape,inplace=True)
-    print(val)
 
     h_defect = val.data
     h_defect = h_defect.reshape(-1,*h_defect.shape[-d:])
@@ -324,7 +282,6 @@ def get_tiles(W:np.ndarray,
 
     reshape = inds1 + inds2
     val = val.transpose(*reshape,inplace=True)
-    print(val)
 
     v_direct = val.data
     v_direct = v_direct.reshape(-1,*v_direct.shape[-d:])
@@ -355,7 +312,6 @@ def get_tiles(W:np.ndarray,
         reshape = reshape + (f'k{2*d},{2*i+1}',)
 
     val = val.transpose(*reshape,inplace=True)
-    print(val)
 
     v_defect = val.data
     v_defect = v_defect.reshape(-1,*v_defect.shape[-d:])
