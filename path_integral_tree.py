ba49a0b6e3b200b35810ceea6c97680373fd26d8
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-28 23:06:50 +0100

    adding in truncation functionality

--- path_integral_tree.py
+++ path_integral_tree.py
@@ -1,85 +1,127 @@
 import matplotlib.pyplot as plt
 from path_integral import list_generator
 from P_generator import Exponential_Map
-from path_integral import path_integral as PI
 from time import time
 import numpy as np
 import pandas as pd
 import math
+import os
+import re
 
 def main():
+
     e = 1e-7
     q = 2
-    W_path = 'data/FoldedTensors/DU_2_33296628.csv'
-    P_path = 'data/FoldedPertubations/P_2_866021931.csv'
-    W = np.loadtxt(W_path,delimiter=',',dtype='complex_')
-    G = np.loadtxt(P_path,delimiter=',',dtype='complex_')
-    P = Exponential_Map(e,G)
-    PW = np.einsum('ab,bc->ac',P,W).reshape([q**2,q**2,q**2,q**2])
+    tspan = 13
+    k = 3
 
-    tspan = 12
+    generate_data(q,tspan,e,k)
 
-    tree = Tree()
-    df = pd.DataFrame()
+class Node:
+    def __init__(self,data:complex=None):
+        self.left = None
+        self.right = None
+        self.data = data
 
-    start = time()
+class Tree:
+    def __init__(self,root=Node()):
+        self.root = root
+        self.current = self.root
 
-    err = pd.Series()
+    def move_left(self):
+        if not self.current.left:
+            self.current.left = Node()
+        self.current = self.current.left
 
-    for T in range(2*tspan+1):
-        t = float(T)/2
+    def move_right(self):
+        if not self.current.right:
+            self.current.right = Node()
+        self.current = self.current.right
+    
+    def assign_data(self,data:complex):
+        self.current.data = data
+        # print(f'                        DATA ASSIGNED')
 
-        data = np.array([])
-        inds = np.array([])
+    def return_root(self):
+        self.current = self.root     
 
-        for x in range(-T+1,T+1):
-            x = float(x)/2
-            inds = np.append(inds,x)
-            data = np.append(data,[path_integral(x,t,PW,tree)])
+def generate_data(q:int,
+                  tspan:int,
+                  e:float,
+                  k:int = 0):
+    
+    if k:
+        path = 'data/PathIntegralTreeTruncated'
+    else:
+        path = 'data/PathIntegralTree'
 
-        print(f'computed for T = {t}s')
+    rstr = r'DU_' + str(q) + r'_([0-9]*).csv'
+    rx = re.compile(rstr)
 
-        s = pd.Series(data,inds,name=t)        
-        df = pd.concat([df, s.to_frame().T])
-        err[t] = np.abs(sum(data))
+    for _, _, files in os.walk('data/FoldedTensors'):
+        for file in files[:1]:
 
-    df = df.reindex(sorted(df.columns,key=lambda num: float(num)), axis=1)
-    df = df.fillna(0)
+            res = rx.match(file)
+            seed = res.group(1)
+    
+            W = np.loadtxt(f'data/FoldedTensors/DU_{q}_{seed}.csv',
+                            delimiter=',',dtype='complex_')
 
-    # plt.imshow(df.iloc[::-1],cmap='hot',interpolation='nearest')
-    # plt.show()
-    # plt.close()
+            G = np.loadtxt(f'data/FoldedPertubations/P_{q}_866021931.csv',
+                           delimiter=',',dtype='complex_')
 
-    # end = time()
+            P = Exponential_Map(e,G)
+            PW = np.einsum('ab,bc->ac',P,W).reshape([q**2,q**2,q**2,q**2])
 
-    # print(f'Time for tree path integral: {end-start}')  
+            tree = Tree()
+            df = pd.DataFrame()
 
-    # df = df.to_numpy(dtype='complex_')
+            start = time()
 
-    # cross_section = list(np.abs(df[5,:])**2)
-    # space = list((np.array(range(len(cross_section)))  + 1 - (len(cross_section)/2))/2)
-    # space_even = space[::2]
-    # space_odd = space[1::2]
-    # cross_section_even = cross_section[::2]
-    # cross_section_odd = cross_section[1::2]
+            err = pd.Series()
 
-    # plt.scatter(space_odd, cross_section_odd, marker="d", color='k', s=5)
+            for T in range(2*tspan+1):
+                t = float(T)/2
 
-    print(err)
+                data = np.array([])
+                inds = np.array([])
 
-    # plt.show()
+                for x in range(-T+1,T+1):
+                    x = float(x)/2
+                    inds = np.append(inds,x)
+                    data = np.append(data,[path_integral(x,t,PW,tree,k=k)])
 
+                print(f'computed for T = {t}s')
 
+                s = pd.Series(data,inds,name=t)        
+                df = pd.concat([df, s.to_frame().T])
+                err[t] = np.abs(sum(data))
 
-    # tree.return_root()
+            df = df.reindex(sorted(df.columns,key=lambda num: float(num)), axis=1)
+            df = df.fillna(0)
+            print(df)
 
-    # printTree(tree.current)      
+            end = time()
+
+            print(f'\nTime for tree path integral: {end-start}\n') 
+
+            try:
+                df.to_csv(path+f'/{q}_{seed}_{e}.csv')
+            except:
+                os.mkdir(path)
+                df.to_csv(path+f'/{q}_{seed}_{e}.csv')
+
+            try:
+                err.to_csv(path+f'/charge_conservation/QC_{q}_{seed}_{e}.csv')
+            except:
+                os.mkdir(path+'/charge_conservation')
+                err.to_csv(path+f'/charge_conservation/QC_{q}_{seed}_{e}.csv')
 
 def path_integral(x:float,
                   t:float,
                   W:np.ndarray,
-                  tree,
-                  k:int = None):
+                  tree:Tree,
+                  k:int = 0):
     
     def transfer_matrix(a:np.ndarray,
                         l:int,
@@ -102,15 +144,11 @@ def path_integral(x:float,
             defect = W[0,:,0,:]
 
         if ((int(2*(t+x))%2 == 0) and horizontal) or ((int(2*(t+x))%2 != 0) and not horizontal):
-            # print('    TERMINATING WITH DIRECT')
-            # print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
             for _ in range(l):
                 a = np.einsum('ab,b->a',direct,a)
             return np.einsum('a,a->',b,a)
 
         else:
-            # print('      TERMINATING WITH DEFECT')
-            # print(f'      Computing transfer matrix len {l}, Horizontal? {horizontal}')
             for _ in range(l-1):
 
                 if horizontal:
@@ -155,20 +193,15 @@ def path_integral(x:float,
         if len(v) == len(h):
             for i in range(len(v)-1):
                 search_tree(h[i])
-                # print(f'    Followed horizontal transfer matrix of length {h[i]}')
                 search_tree(v[i],horizontal=False)
-                # print(f'    Followed vertical transfer matrix of length {v[i]}')
             search_tree(h[-1])
-            # print(f'    Followed horizontal transfer matrix of length {h[-1]}')
 
             a = tree.current.data
 
         elif len(h) > len(v):
             for i in range(len(v)):
                 search_tree(h[i])
-                # print(f'    Followed horizontal transfer matrix of length {h[i]}')
                 search_tree(v[i],horizontal=False)
-                # print(f'    Followed vertical transfer matrix of length {v[i]}')
 
             a = tree.current.data
 
@@ -197,13 +230,10 @@ def path_integral(x:float,
 
     while n <= k:
 
-        # print(f'\nComputing skeleton diagram for {n} turns!')
-
         try:
             l1, l2 = horizontal_data[n], vertical_data[n]
             for h in l1:
                 for v in l2:
-                    # print(f'\n   Diagram h:{h} and v:{v}')
                     sum += skeleton(h,v,a)
         except:pass
             
@@ -211,48 +241,19 @@ def path_integral(x:float,
             l1, l2 = horizontal_data[n+1], vertical_data[n]
             for h in l1:
                 for v in l2:
-                    # print(f'\n   Diagram h:{h} and v:{v}')
                     sum += skeleton(h,v,a)
         except:pass
-                        
-        try:
-            l1, v = horizontal_data[n], vertical_data[0]
-            for h in l1:
-                # print(f'\n   Diagram h:{h} and v:[]')
-                sum += skeleton(h,[],a)
-        except:pass
+
+        if n == 1:        
+            try:
+                l1, v = horizontal_data[n], vertical_data[0]
+                for h in l1:
+                    sum += skeleton(h,[],a)
+            except:pass
 
         n += 1
 
     return sum
-
-class Node:
-    def __init__(self,data:complex=None):
-        self.left = None
-        self.right = None
-        self.data = data
-
-class Tree:
-    def __init__(self,root=Node()):
-        self.root = root
-        self.current = self.root
-
-    def move_left(self):
-        if not self.current.left:
-            self.current.left = Node()
-        self.current = self.current.left
-
-    def move_right(self):
-        if not self.current.right:
-            self.current.right = Node()
-        self.current = self.current.right
-    
-    def assign_data(self,data:complex):
-        self.current.data = data
-        # print(f'                        DATA ASSIGNED')
-
-    def return_root(self):
-        self.current = self.root
 
 def printTree(root:Node, level:int=0):
 
