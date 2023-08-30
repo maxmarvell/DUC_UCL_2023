14f8d891402e9e232adfacd2452ab5b3fee05976
maxmarvell <max.marvell@hotmail.co.uk>
2023-08-30 15:22:13 +0100

    saving data to data folder instead

--- tile_generator.py
+++ tile_generator.py
@@ -1,27 +1,25 @@
+from P_generator import Exponential_Map
 import numpy as np
 import quimb.tensor as qtn
-from scipy.linalg import expm 
-
 
 def main():
 
-    #used e = 0.0000004 as reminder
     q = 2
     e = 0.0000004
     d = 5
     
-    W_path = "./Sample_Tensor.csv"
-    P_path = "./Sample_Perturbation.csv"
-    W = np.loadtxt(W_path,delimiter=',',dtype='complex_')
-    G = np.loadtxt(P_path,delimiter=',',dtype='complex_')
-    print("\n")
-    P = expm(1j*e*G)
+    W = np.loadtxt('data/FoldedTensors/DU_2_3806443768.csv',
+                   delimiter=',',
+                   dtype='complex_')
+    
+    G = np.loadtxt('data/FoldedPertubations/P_2_866021931.csv',
+                   delimiter=',',
+                   dtype='complex_')
+    
+    P = Exponential_Map(e,G)
     PW = np.einsum('ab,bc->ac',P,W).reshape([q**2,q**2,q**2,q**2])
 
     get_tiles(PW, d)
-
-
-
 
 
 def get_tiles(W:np.ndarray,
@@ -32,12 +30,8 @@ def get_tiles(W:np.ndarray,
         horizontal direct, vertical defect, vertical direct for a d dimensional
         case
 
-        N.B. convention for index labelling is "kx,y where (x,y) is the bond coordinate"
+        N.B. convention for index labelling is 'kx,y where (x,y) is the bond coordinate'
     '''
-
-    #tiles = dict()
-
-
 
     # HORIZONTAL DIRECT TILE
 
@@ -57,7 +51,6 @@ def get_tiles(W:np.ndarray,
                     for i in range(d_h):
                         tensors = np.append(tensors,[qtn.Tensor(W[0,:,:,0],inds=(f'k{2*i+1},2',f'k{2*i+2},1',f'k{2*i},1'))])
 
-
                 TN = qtn.TensorNetwork(tensors)
                 val = TN.contract()
 
@@ -69,21 +62,18 @@ def get_tiles(W:np.ndarray,
                     reshape = reshape + (f'k0,{2*i+1}',)
 
                 val = val.transpose(*reshape,inplace=True)
-                
-
+            
                 h_direct = val.data
                 h_direct = h_direct.reshape(-1,*h_direct.shape[-d_v:])
                 h_direct = h_direct.reshape(*h_direct.shape[:1],-1)
 
-                np.savetxt(f"./Tile_Zoo/h_direct_{d_h}x{d_v}", h_direct, delimiter=",")
+                np.savetxt(f'TileZoo/h_direct_{d_h}x{d_v}',h_direct,delimiter=',')
 
             else:
                 h_direct = W[0,:,:,0]
                 for i in range(d_h-1):
                     h_direct = np.einsum('ab,bc->ac',h_direct,W[0,:,:,0])
-                np.savetxt(f"./Tile_Zoo/h_direct_{d_h}x{d_v}", h_direct, delimiter=",")
-
-
+                np.savetxt(f'TileZoo/h_direct_{d_h}x{d_v}',h_direct,delimiter=',')
 
     # HORIZONTAL DEFECT TILE
 
@@ -102,7 +92,6 @@ def get_tiles(W:np.ndarray,
                     
                     tensors = np.append(tensors,[qtn.Tensor(W[:,0,:,:],inds=(f'k{2*(d_h-1)+1},{2*j+2}',f'k{2*(d_h-1)},{2*j+1}',f'k{2*(d_h-1)+1},{2*j}'))])
 
-
                 TN = qtn.TensorNetwork(tensors)
                 val = TN.contract()
 
@@ -114,18 +103,15 @@ def get_tiles(W:np.ndarray,
                     reshape = reshape + (f'k0,{2*i+1}',)
 
                 val = val.transpose(*reshape,inplace=True)
-                
 
                 h_defect = val.data
                 h_defect = h_defect.reshape(-1,*h_defect.shape[-d_v:])
                 h_defect = h_defect.reshape(*h_defect.shape[:1],-1)
 
-                np.savetxt(f"./Tile_Zoo/h_defect_{d_h}x{d_v}", h_defect, delimiter=",")
+                np.savetxt(f'TileZoo/h_defect_{d_h}x{d_v}',h_defect,delimiter=',')
 
             else:
-                np.savetxt(f"./Tile_Zoo/h_defect_{d_h}x{d_v}", W[:,0,:,0], delimiter=",")
-
-
+                np.savetxt(f'TileZoo/h_defect_{d_h}x{d_v}', W[:,0,:,0],delimiter=',')
 
     ### VERTICAL DIRECT TILE
 
@@ -152,20 +138,17 @@ def get_tiles(W:np.ndarray,
 
                 val = val.transpose(*reshape,inplace=True)
                 
-
                 v_direct = val.data
                 v_direct = v_direct.reshape(-1,*v_direct.shape[-d_h:])
                 v_direct = v_direct.reshape(*v_direct.shape[:1],-1)
 
-                np.savetxt(f"./Tile_Zoo/v_direct_{d_h}x{d_v}", v_direct, delimiter=",")
+                np.savetxt(f'TileZoo/v_direct_{d_h}x{d_v}',v_direct,delimiter=',')
 
             else:
                 v_direct = W[:,0,0,:]
                 for i in range(d_v-1):
                     v_direct = np.einsum('ab,bc->ac',v_direct,W[:,0,0,:])
-                np.savetxt(f"./Tile_Zoo/v_direct_{d_h}x{d_v}", v_direct, delimiter=",")
-
-
+                np.savetxt(f'TileZoo/v_direct_{d_h}x{d_v}',v_direct,delimiter=',')
 
     ### VERTICAL DEFECT TILE
 
@@ -196,18 +179,14 @@ def get_tiles(W:np.ndarray,
 
                 val = val.transpose(*reshape,inplace=True)
                 
-
                 v_defect = val.data
                 v_defect = v_defect.reshape(-1,*v_defect.shape[-d_h:])
                 v_defect = v_defect.reshape(*v_defect.shape[:1],-1)
 
-                np.savetxt(f"./Tile_Zoo/v_defect_{d_h}x{d_v}", v_defect, delimiter=",")
+                np.savetxt(f'data/TileZoo/v_defect_{d_h}x{d_v}',v_defect,delimiter=',')
 
             else:
-                np.savetxt(f"./Tile_Zoo/v_defect_{d_h}x{d_v}", W[0,:,0,:], delimiter=",")
-
-   
-
+                np.savetxt(f'data/TileZoo/v_defect_{d_h}x{d_v}',W[0,:,0,:],delimiter=',')
 
 if __name__ == '__main__':
     main()
